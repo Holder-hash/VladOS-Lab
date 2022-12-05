23d3a29e25ec9822241557917bd8007462e215c6
h0ler <63481484+Holder-hash@users.noreply.github.com>
2022-12-05 23:29:44 +0300

    Add files via upload

---
+++ games/js.js
@@ -0,0 +1,13 @@
+var one_call = false;
+ 
+function onetime(){
+	if (one_call == false){
+		alert('Функция «func_one» выполнена');
+	}
+	one_call = true;
+}
+
+function myFunc(){
+event.target.src="../anon.png"
+document.getElementsByClassName('V')[0].style="cursor: default";
+}
